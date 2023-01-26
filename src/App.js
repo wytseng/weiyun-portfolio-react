0ecd0dd41b6792673ef42b46594e3e65852bbeb3
Steph T <wytseng@uci.edu>
2023-01-25 23:29:16 -0800

    add all needed component folders (empty) finished about me

--- src/App.js
+++ src/App.js
@@ -1,15 +1,54 @@
-import NavMenu from './components/NavMenu';
-import Home from './components/Home'
-import './App.css';
+import { ThemeProvider  } from 'react-bootstrap';
+import NavHeader from './components/NavHeader';
+import About from './components/Aboutme';
+import Portfolio from './components/Porfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
+import Footer from './components/Footer';
+import { useState } from 'react';
 
 function App() {
+
+  const pages = [
+    { name: "About Me" },
+    { name: "Portfolio" },
+    { name: "Contact" },
+    { name: "Resume" }
+  ]  
+
+  const [ currentPage, setCurrentPage ] = useState(pages[0]);
+
+  const renderPage = () => {
+    switch (currentPage.name) {
+      case "About Me":
+        return <About />
+      case "Portfolio":
+        return <Portfolio />
+      case "Contact": 
+        return <Contact />
+      case 'Resume':
+        return <Resume />
+      default:
+        return <About /> 
+    }
+  }
+
   return (
-    <div className="App">
-      <NavMenu />
-      <main>
-        <Home />
-      </main>
-    </div>
+    <ThemeProvider
+      breakpoints={['xxl', 'xl', 'lg', 'md', 'sm', 'xs']}
+    > 
+      <div>
+        <NavHeader 
+          pages={pages}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+        />
+        <main>
+          {renderPage()}
+        </main>
+        <Footer />
+      </div>
+    </ThemeProvider>
   );
 }
 
